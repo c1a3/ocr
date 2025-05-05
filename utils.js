5268dd89d0502f2775d4daebe544f26c4c16b090
c1a3 <154661452+c1a3@users.noreply.github.com>
2025-05-05 14:57:13 +0000

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
 function setProgress(message) {
   document.getElementById('progress').textContent = message;
 }
+
+function clearProgress() {
+  document.getElementById('progress').textContent = '';
+}
