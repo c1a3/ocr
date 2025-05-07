f73dd865235fe9e4006704bcf5a5cc33178c95bd
c1a3 <154661452+c1a3@users.noreply.github.com>
2025-05-07 16:38:44 +0000

    Update ocr.js

--- ocr.js
+++ ocr.js
@@ -1,2 +1,6 @@
 pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js';
 
+async function processPDF(file) {
+  const arrayBuffer = await file.arrayBuffer();
+  const pdf = await pdfjsLib.getDocument(arrayBuffer).promise;
+  let text
